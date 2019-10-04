58cf185b78de1d6f950847304f453e85f34bacfa
Jordan Tran <kim.tran94@gmail.com>
2019-10-03 19:35:07 -0500

    Add go back button

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,6 +4,14 @@ export default {
     toggleDrawer() {
       this.$emit('toggle-drawer')
     },
+
+    goBack() {
+      if(this.$el.classList.contains('v-toolbar--collapsed')) {
+        this.$vuetify.goTo('#app')
+      } else {
+        this.$router.back()
+      }
+    },
   },
 }
 </script>
@@ -18,11 +26,25 @@ export default {
     <v-app-bar-nav-icon @click.stop="toggleDrawer" />
 
     <v-toolbar-title>{{ $route.meta.title }}</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <v-btn icon @click="goBack">
+      <v-icon>mdi-chevron-left-circle</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
 <style scoped>
+  .v-app-bar .mdi-chevron-left-circle {
+    transition: all 0.1s ease;
+  }
+
   .v-toolbar.v-toolbar--collapsed {
-    max-width: 80px;
+    max-width: 104px;
+  }
+
+  .v-toolbar.v-toolbar--collapsed .mdi-chevron-left-circle {
+    transform: rotate(90deg);
   }
 </style>
